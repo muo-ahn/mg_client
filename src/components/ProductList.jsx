27c48b4f90759881b0613d431961aa70016ea4cf
muo-ahn <dksehdgur321@gmail.com>
2024-09-27 16:32:18 +0900

    update css & fixing search API calling

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -88,7 +88,7 @@ const ProductList = ({ searchQuery }) => {
     if (searchQuery) {
       const fetchSearchResults = async () => {
         try {
-          const response = await fetch(`https://medakaauction.com/search?query=${searchQuery}/`, {
+          const response = await fetch(`https://medakaauction.com/search?query=${searchQuery}`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
