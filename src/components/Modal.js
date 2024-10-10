34482410ffa11e83d3d5457e0528598773be680f
muo-ahn <dksehdgur321@gmail.com>
2024-10-10 13:32:02 +0900

    modify Modal component to hide as default

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,6 @@
-// src/components/Modal.js
-
 import React, { useEffect } from 'react';
 
 const Modal = ({ show, onClose, children }) => {
-  // Close modal on ESC key press
   useEffect(() => {
     const handleEscape = (event) => {
       if (event.key === 'Escape') {
@@ -20,32 +17,27 @@ const Modal = ({ show, onClose, children }) => {
     return () => document.removeEventListener('keydown', handleEscape);
   }, [show, onClose]);
 
-  // Close modal if clicked outside of the content area
   const handleOutsideClick = (e) => {
     if (e.target.classList.contains('modal-overlay')) {
       onClose();
     }
   };
 
+  if (!show) {
+    return null;
+  }
+
   return (
     <div
-      className={`modal-overlay fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50 ${show ? 'show' : ''}`} // Add the 'show' class dynamically
+      className={`modal-overlay ${show ? 'show' : ''}`}
       onClick={handleOutsideClick}
     >
       <div
-        className={`modal-content bg-white rounded-lg p-6 relative transition-transform transform-gpu duration-300 ${show ? 'show' : ''}`} // Add the 'show' class dynamically
+        className={`modal-content ${show ? 'show' : ''}`}
         role="dialog"
         aria-modal="true"
-        aria-labelledby="modal-title"
-        tabIndex="-1"
       >
-        <button
-          onClick={onClose}
-          className="absolute top-0 right-0 mt-2 mr-2 text-gray-500 hover:text-gray-700"
-          aria-label="Close modal"
-        >
-          &times;
-        </button>
+        <button onClick={onClose}>&times;</button>
         {children}
       </div>
     </div>
